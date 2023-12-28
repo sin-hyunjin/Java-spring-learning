0c6883289a9af4cfe0121438b240c3401e4fd517
Hyun jin <blackduvet52@gmail.com>
2023-12-29 01:01:03 +0900

    Comment 주석기능

---
+++ src/CommentJava.java
@@ -0,0 +1,16 @@
+public class CommentJava {
+
+    // // = 한줄 주석
+    // /* */ = 여러줄 주석
+    // psvm = public static void main(String[] args){} 자동완성 약자
+    public static void main(String[] args) {
+        System.out.println("hello java");
+        // System.out.println("hello java");
+
+        /*
+        System.out.println("hello java");
+        System.out.println("hello java");
+         */
+
+    }
+}
