ec5b84d7b0ae161e98d80e96839858fb92214ee4
Hyun jin <blackduvet52@gmail.com>
2023-12-29 01:36:51 +0900

    번수 선언과 초기화

---
+++ src/variable/Var3.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var3 {
+
+    public static void main(String[] args) {
+        int a; //변수선언
+        a = 10; //변수 초기화 a(10)
+        System.out.println(a);
+        a = 50; //변수 값 변경 a(10 -> 50)
+        System.out.println(a); // 변수의 값을 변경하면 변수에 들어있던 기존 값은 삭제된다.
+    }
+}
---
+++ src/variable/Var6.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var6 {
+
+    public static void main(String[] args) {
+
+        int a;
+        // System.out.println(a);
+        //java: variable a might not have been initialized
+        //컴퍼일 에러
+    }
+}
