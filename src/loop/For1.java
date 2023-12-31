c31576e50de67ab1fa589afcf0d76c5129fcb122
Hyun jin <blackduvet52@gmail.com>
2023-12-31 20:42:20 +0900

    반복문 for문   - for(초기식; 조건식; 증감식;){코드}

---
+++ src/loop/For1.java
@@ -0,0 +1,13 @@
+package loop;
+/* for문
+    - for문은 주로 반복 횟수기 정해져 있을 때 사용
+    - for(초기식; 조건식; 증감식;){코드}
+ */
+public class For1 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
