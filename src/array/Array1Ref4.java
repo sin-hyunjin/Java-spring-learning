8dc4c78751d7925fbddd24a355085402599c9b25
Hyun jin <blackduvet52@gmail.com>
2024-01-02 14:40:28 +0900

    배열 리팩토링

---
+++ src/array/Array1Ref4.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Array1Ref4 {
+
+    public static void main(String[] args) {
+        /* Array1Ref
+        더 간략하게 초기화 하는법
+         */
+        int[] students = {90, 80, 70, 60, 50};
+
+        //변수 값 사용
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("학생" + (i + 1) + " 점수: " + students[i]);
+        }
+    }
+}
