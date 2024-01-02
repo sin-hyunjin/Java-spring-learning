623c51f37bcc3a8d6f4510c8440a2e2858bef359
Hyun jin <blackduvet52@gmail.com>
2024-01-02 18:57:02 +0900

    method 문제풀이

--- src/method/ex/MethodEx3Ref.java
+++ src/method/ex/MethodEx3Ref.java
@@ -4,18 +4,20 @@ public class MethodEx3Ref {
     public static void main(String[] args) {
         int balance = 10000;
 
-        balance = deposit(balance, 1000);
-        balance = withdraw(balance, 2000);
+        // 대목차느낌
+        balance = deposit(balance, 1000); //입금 1000
+        balance = withdraw(balance, 2000); //출금 2000
 
         System.out.println("최종 잔액: " + balance + "원");
     }
 
-    public static int deposit(int balance, int amount) {
+    // 입금 메소드 1000
+    public static int deposit(int balance, int amount) { // balance = 입금, amount = 잔액
         balance += amount;
         System.out.println(amount + "원을 입금하였습니다. 현재 잔액: " + balance + "원");
         return balance;
     }
-
+    // 출금 메소드 2000
     public static int withdraw(int balance, int amount) {
         if (balance >= amount) {
             balance -= amount;
--- src/method/ex/MethodEx4.java
+++ src/method/ex/MethodEx4.java
@@ -3,7 +3,13 @@ package method.ex;
 import java.util.Scanner;
 
 public class MethodEx4 {
+    /*
+    deposit 메소드: 입금 기능을 처리하며, 입금액을 현재 잔액에 더한 후 결과를 반환함
 
+    withdraw 메소드: 출금 기능을 처리하며, 출금액이 현재 잔액보다 작거나 같을 경우 출금하고 결과를 반환함. 그렇지 않으면 잔액이 부족하다는 메시지를 출력함
+
+    main 메소드: 사용자로부터 메뉴를 선택받아 해당하는 기능을 수행하고, 잔액을 출력함. 종료 메뉴를 선택하면 프로그램이 종료됨
+     */
     public static void main(String[] args) {
         int balance = 0;
         Scanner scanner = new Scanner(System.in);
@@ -14,8 +20,8 @@ public class MethodEx4 {
             System.out.println("---------------------------------");
             System.out.print("선택: ");
 
-            int choice = scanner.nextInt();
-            int amount;
+            int choice = scanner.nextInt(); //사용자가 선택한 숫자
+            int amount; //사용자가 입력한 금액
 
             switch (choice) {
                 case 1:
