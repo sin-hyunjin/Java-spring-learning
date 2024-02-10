e14b486f9f2a04b98a03bcdb126da4ada447a67c
Hyun jin <blackduvet52@gmail.com>
2024-02-10 23:52:50 +0900

    접근 제어자

---
+++ 2.java-basic/java-basic/src/access/Speaker.java
@@ -0,0 +1,31 @@
+package access;
+
+/**
+ * 스피커 객체를 만들어보자.
+ * 스피커는 음량을 높이고, 내리고, 현재 음량을 확인할 수 있는 단순한 기능을 제공한다. 요구사항 대로 스피커의 음량은 100까지만 증가할 수 있다. 절대 100을 넘어가면 안된다.
+ */
+public class Speaker {
+    private int volume; // Speaker 내부에서만 접근이 가능하다
+
+    Speaker(int volume) {
+        this.volume = volume;
+    }
+
+    void volumeUp() {
+        if (volume >= 100) {
+            System.out.println("음량을 증가할 수 없습니다. 최대 음량입니다.");
+        } else {
+            volume += 10;
+            System.out.println("음량을 10 증가합니다.");
+        }
+    }
+
+    void volumeDown() {
+        volume -= 10;
+        System.out.println("음량을 10 감소합니다.");
+    }
+
+    void showVolume() {
+        System.out.println("volume 현재 음량 = " + volume);
+    }
+}
---
+++ 2.java-basic/java-basic/src/access/SpeakerMain.java
@@ -0,0 +1,24 @@
+package access;
+
+public class SpeakerMain {
+    /**초기 음량 값을 90으로 지정했다. 그리고 음량을 높이는 메서드를 여러번 호출했다.
+
+     */
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker(90);
+        speaker.showVolume();
+        speaker.volumeUp();
+        speaker.showVolume();
+        speaker.volumeUp();
+        speaker.showVolume();
+`
+        //필드에 직접 접근
+        System.out.println("volume 필드 직접 접근 수정");
+//        speaker.volume = 200;
+        speaker.showVolume();
+        /**`Speaker` 객체를 사용하는 사용자는 `Speaker` 의 `volume` 필드와 메서드에 모두 접근할 수 있다.
+         앞서 `volumeUp()` 과 같은 메서드를 만들어서 음량이 100을 넘지 못하도록 기능을 개발했지만 소용이 없다. 왜냐하 면 `Speaker` 를 사용하는 입장에서는 `volume` 필드에 직접 접근해서 원하는 값을 설정할 수 있기 때문이다.
+         이런 문제를 근본적으로 해결하기 위해서는 `volume` 필드의 외부 접근을 막을 수 있는 방법이 필요하다.
+         */
+    }
+}
